b9e7b0493500302c4abbdaff3950e7e251e9ce2d
yeqiang <289632785@qq.com>
2020-09-18 13:45:47 +0800

    sync

--- 02.chroot.sh
+++ 02.chroot.sh
@@ -1,11 +1,45 @@
+#!/bin/bash
+# 6.2. Preparing Virtual Kernel File Systems
+mkdir -pv $LFS/{dev,proc,sys,run}
+# 6.2.1. Creating Initial Device Nodes
+mknod -m 600 $LFS/dev/console c 5 1
+mknod -m 666 $LFS/dev/null c 1 3
+# 6.2.2. Mounting and Populating /dev
+mount -v --bind /dev $LFS/dev
+# 6.2.3. Mounting Virtual Kernel File Systems
+mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mount -vt proc proc $LFS/proc
+mount -vt sysfs sysfs $LFS/sys
+mount -vt tmpfs tmpfs $LFS/run
+if [ -h $LFS/dev/shm ]; then
+mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+fi
+
+
+#文中未提到，chroot前配置lib lib64 bin目录，指向tools
+####################################################
+# cd $LFS
+# mkdir usr/lib
+# mkdir usr/lib64
+# mkdir usr/bin
+
+# ln -s usr/bin bin
+# ln -s usr/lib lib
+# ln -s usr/lib64 lib64
+
+# (
+# 	cd usr/lib64
+# 	ls ../../tools/lib64/ | xargs -i ln -svf "../../tools/lib64/"{}
+# )
+
+####################################################
 
 
 # 6.4. Entering the Chroot Environment
 # by root user
-chroot "$LFS" /bin/env -i \
-	HOME=/root \
-	TERM="$TERM" \
-	PS1='\u:\w\$ ' \
-	PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
-	/bin/bash --login +h
-
+chroot "$LFS" /tools/bin/env -i \
+    HOME=/root \
+    TERM="$TERM" \
+    PS1='\u:\w\$ ' \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+    /tools/bin/bash --login +h
--- 03.build_lfs_02.sh.sh
+++ 03.build_lfs_02.sh.sh
@@ -1,160 +1,96 @@
 #!/bin/bash
-# run as root
-exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
-umask 022
-export LFS=/mnt/lfs
-export LC_ALL=POSIX
-export LFS_TGT=x86_64-lfs-linux-gnu
-export PATH=/tools/bin:/bin:/usr/bin
-export LFS LC_ALL LFS_TGT PATH
+set -x
 
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664 /var/log/lastlog
+chmod -v 600 /var/log/btmp
 
-mkdir -pv $LFS
-mkdir -pv $LFS/usr
-mkdir -pv $LFS/tools
-mkdir -pv $LFS/sources
-# wget --input-file=wget-list --continue --directory-prefix=$LFS/sources
-mkdir -v $LFS/tools
-ln -sv $LFS/tools /
+cd /sources/build/
 
-
-
-cd $LFS/sources
-mkdir build
-cd build
-
-#######################
-# 利用宿主服务gcc进行编译#
-######################
-
-# 5.4. Binutils-2.27 - Pass 1
-tar -jxvf $LFS/sources/binutils-2.27.tar.bz2
+###
 (
-    cd binutils-2.27/
-    mkdir -v build    
-    cd build
-    ../configure --prefix=/tools \
-        --with-sysroot=$LFS \
-        --with-lib-path=/tools/lib \
-        --target=$LFS_TGT \
-        --disable-nls \
-        --disable-werror    
-    make -j12
- 
-    case $(uname -m) in
-    x86_64) mkdir -v /tools/lib && ln -sv lib /tools/lib64 ;;
-    esac
- 
-    make install    
+    cd /lib64
+    ln /tools/lib64/ld-2.25.so ld-linux-x86-64.so.2
+
+    # 需要观察后续这个库是否被重新覆盖！
 )
-# 5.5. GCC-6.3.0 - Pass 1
-tar -jxvf $LFS/sources/gcc-6.3.0.tar.bz2
-(
-    cd gcc-6.3.0/
-    # GCC now requires the GMP, MPFR and MPC packages
-    tar -xf $LFS/sources/mpfr-3.1.5.tar.xz
-    mv -v mpfr-3.1.5 mpfr
-    tar -xf $LFS/sources/gmp-6.1.2.tar.xz
-    mv -v gmp-6.1.2 gmp
-    tar -xf $LFS/sources/mpc-1.0.3.tar.gz
-    mv -v mpc-1.0.3 mpc
-    # The following command will change the location of GCC's default dynamic linker to use the one installed in /tools.
-    # It also removes /usr/include from GCC's include search path.
-    for file in gcc/config/{linux,i386/linux{,64}}.h
-        do
-        cp -uvf $file{,.orig}
-        sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
-        -e 's@/usr@/tools@g' $file.orig > $file
-        echo '
-		#undef STANDARD_STARTFILE_PREFIX_1
-        #undef STANDARD_STARTFILE_PREFIX_2
-        #define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"
-        #define STANDARD_STARTFILE_PREFIX_2 ""' >> $file
-        touch $file.orig
-    done
+###
 
-    # on x86_64 hosts, set the default directory name for 64-bit libraries to “lib”
-    case $(uname -m) in
-    x86_64)
-    sed -e '/m64=/s/lib64/lib/' \
-    -i.orig gcc/config/i386/t-linux64
-    ;;
-    esac
-
-    mkdir -v build    
-    cd build
-	# export CFLAGS="-O2 -fpermissive"
-	# export CXXFLAGS="-O2 -fpermissive"
-    ../configure \
-        --target=$LFS_TGT \
-        --prefix=/tools \
-        --with-glibc-version=2.11 \
-        --with-sysroot=$LFS \
-        --with-newlib \
-        --without-headers \
-        --with-local-prefix=/tools \
-        --with-native-system-header-dir=/tools/include \
-        --disable-nls \
-        --disable-shared \
-        --disable-multilib \
-        --disable-decimal-float \
-        --disable-threads \
-        --disable-libatomic \
-        --disable-libgomp \
-        --disable-libmpx \
-        --disable-libquadmath \
-        --disable-libssp \
-        --disable-libvtv \
-        --disable-libstdcxx \
-        --enable-languages=c,c++
-    
-    make -j12    
-    make install    
-)
-
-# 5.6. Linux-4.9.9 API Headers
-tar -xvf $LFS/sources/linux-4.9.9.tar.xz
+# 6.7. Linux-4.9.9 API Headers
+# tar -xvf /sources/linux-4.9.9.tar.xz
 (
     cd linux-4.9.9/
     make mrproper
     make INSTALL_HDR_PATH=dest headers_install
-    cp -rvf dest/include/* /tools/include
+    find dest/include \( -name .install -o -name ..install.cmd \) -delete
+    cp -rv dest/include/* /usr/include
 )
+# pkg-config 编译报错！！！
+# tar -xvf /sources/pkg-config-0.29.1.tar.gz
+# (
+#     cd pkg-config-0.29.1/
+#     CFLAGS="-Werror=format-nonliteral"
+#     ./configure --prefix=/usr --with-internal-glib
+# )
 
-##############################
-# 利用编译出来的gcc6.3.0进行编译 #
-#############################
-# 比原文提前！
+# # 6.8. Man-pages-4.09 依赖上一个pkg-config!!!
+# tar -xvf /sources/man-db-2.7.6.1.tar.xz
+# (
+#     cd man-db-2.7.6.1/
+#     make install
+# )
 
-CC=$LFS_TGT-gcc
-CXX=$LFS_TGT-g++
-AR=$LFS_TGT-ar
-RANLIB=$LFS_TGT-ranlib
-
-
-# 5.7. Glibc-2.25
-tar -xvf $LFS/sources/glibc-2.25.tar.xz
+# 6.9. Glibc-2.25
+# tar -xvf /sources/glibc-2.25.tar.xz
 (
     cd glibc-2.25/
+    make distclean
+    patch -Np1 -i ../../glibc-2.25-fhs-1.patch
+    case $(uname -m) in
+        x86) ln -s ld-linux.so.2 /lib/ld-lsb.so.3
+        ;;
+        x86_64) ln -s ../lib/ld-linux-x86-64.so.2 /lib64
+        ln -s ../lib/ld-linux-x86-64.so.2 /lib64/ld-lsb-x86-64.so.3
+        ;;
+    esac
     mkdir -v build
     cd build
-    ../configure \
-        --prefix=/tools \
-        --host=$LFS_TGT \
-        --build=$(../scripts/config.guess) \
-        --enable-kernel=2.6.32 \
-        --with-headers=/tools/include \
-        libc_cv_forced_unwind=yes \
-        libc_cv_c_cleanup=yes 
+    ../configure --prefix=/usr --enable-kernel=2.6.32 --enable-obsolete-rpc --enable-stack-protector=strong libc_cv_slibdir=/lib
     make -j12
-    export PATH=/bin:/usr/bin:/tools/bin
+    touch /etc/ld.so.conf
     make install
-    export PATH=/tools/bin:/bin:/usr/bin
+    cp -v ../nscd/nscd.conf /etc/nscd.conf
+    mkdir -pv /var/cache/nscd
+    install -v -Dm644 ../nscd/nscd.tmpfiles /usr/lib/tmpfiles.d/nscd.conf
+    install -v -Dm644 ../nscd/nscd.service /lib/systemd/system/nscd.service
+    mkdir -pv /usr/lib/locale
+    localedef -i cs_CZ -f UTF-8 cs_CZ.UTF-8
+    localedef -i de_DE -f ISO-8859-1 de_DE
+    localedef -i de_DE@euro -f ISO-8859-15 de_DE@euro
+    localedef -i de_DE -f UTF-8 de_DE.UTF-8
+    localedef -i en_GB -f UTF-8 en_GB.UTF-8
+    localedef -i en_HK -f ISO-8859-1 en_HK
+    localedef -i en_PH -f ISO-8859-1 en_PH
+    localedef -i en_US -f ISO-8859-1 en_US
+    localedef -i en_US -f UTF-8 en_US.UTF-8
+    localedef -i es_MX -f ISO-8859-1 es_MX
+    localedef -i fa_IR -f UTF-8 fa_IR
+    localedef -i fr_FR -f ISO-8859-1 fr_FR
+    localedef -i fr_FR@euro -f ISO-8859-15 fr_FR@euro
+    localedef -i fr_FR -f UTF-8 fr_FR.UTF-8
+    localedef -i it_IT -f ISO-8859-1 it_IT
+    localedef -i it_IT -f UTF-8 it_IT.UTF-8
+    localedef -i ja_JP -f EUC-JP ja_JP
+    localedef -i ru_RU -f KOI8-R ru_RU.KOI8-R
+    localedef -i ru_RU -f UTF-8 ru_RU.UTF-8
+    localedef -i tr_TR -f UTF-8 tr_TR.UTF-8
+    localedef -i zh_CN -f GB18030 zh_CN.GB18030
 )
-rm -fr glibc-2.25/
+
 
 # 5.8. Libstdc++-6.3.0
-# tar -xvf $LFS/sources/gcc-6.3.0.tar.bz2
+# # tar -xvf /sources/gcc-6.3.0.tar.bz2
 (
     cd gcc-6.3.0/
 	cd build
@@ -170,14 +106,21 @@ rm -fr glibc-2.25/
     make -j12
     make install
 )
+# rm -fr gcc-6.3.0
 
 
+##############################
+# 利用编译出来的gcc6.3.0进行编译 #
+#############################
 
+CC=$LFS_TGT-gcc
+CXX=$LFS_TGT-g++
+AR=$LFS_TGT-ar
+RANLIB=$LFS_TGT-ranlib
 
 # 5.9. Binutils-2.27 - Pass 2
 # 利用编译出来的gcc6.3.0编译binutils
-rm -fr binutils-2.27/
-tar -xvf $LFS/sources/binutils-2.27.tar.bz2
+# tar -xvf /sources/binutils-2.27.tar.bz2
 (
     cd binutils-2.27/
     mkdir -v build
@@ -188,22 +131,23 @@ tar -xvf $LFS/sources/binutils-2.27.tar.bz2
     make install
     make -C ld clean
     make -C ld LIB_PATH=/usr/lib:/lib
-    cp -vf ld/ld-new /tools/bin
+    cp -v ld/ld-new /tools/bin
 )
+# rm -fr binutils-2.27/
 
 # 5.10. GCC-6.3.0 - Pass 2
 # 利用编译出来的gcc 6.3.0 重新编译gcc-6.3.0，并覆盖安装
 # glibc libstdc++包含在 gcc 6.3.0中
 rm -fr gcc-6.3.0/
-tar -xvf $LFS/sources/gcc-6.3.0.tar.bz2
+# tar -xvf /sources/gcc-6.3.0.tar.bz2
 (
     cd gcc-6.3.0/
     # GCC now requires the GMP, MPFR and MPC packages
-    tar -xf $LFS/sources/mpfr-3.1.5.tar.xz
+    # tar -xf /sources/mpfr-3.1.5.tar.xz
     mv -v mpfr-3.1.5 mpfr
-    tar -xf $LFS/sources/gmp-6.1.2.tar.xz
+    # tar -xf /sources/gmp-6.1.2.tar.xz
     mv -v gmp-6.1.2 gmp
-    tar -xf $LFS/sources/mpc-1.0.3.tar.gz
+    # tar -xf /sources/mpc-1.0.3.tar.gz
     mv -v mpc-1.0.3 mpc
 
     cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
@@ -211,7 +155,7 @@ tar -xvf $LFS/sources/gcc-6.3.0.tar.bz2
 
     for file in gcc/config/{linux,i386/linux{,64}}.h
     do
-        cp -uvf $file{,.orig}
+        cp -uv $file{,.orig}
         sed -e 's@/lib\(64\)\?\(32\)\?/ld@/tools&@g' \
         -e 's@/usr@/tools@g' $file.orig > $file
         echo '
@@ -239,11 +183,12 @@ tar -xvf $LFS/sources/gcc-6.3.0.tar.bz2
     make install 
     ln -svf gcc /tools/bin/cc
 )
+# rm -fr gcc-6.3.0/
 
 # 此后所有软件均采用重新编译出来的gcc6.3.0编译
 
 # 5.11. Tcl-core-8.6.6
-tar -xvf $LFS/sources/tcl-core8.6.6-src.tar.gz
+# tar -xvf /sources/tcl-core8.6.6-src.tar.gz
 (
     cd tcl8.6.6/unix
     ./configure --prefix=/tools
@@ -254,12 +199,13 @@ tar -xvf $LFS/sources/tcl-core8.6.6-src.tar.gz
     make install-private-headers
     ln -sv tclsh8.6 /tools/bin/tclsh
 )
+# rm -fr tcl8.6.6/
 
 # 5.12. Expect-5.45
-tar -xvf $LFS/sources/expect5.45.tar.gz
+# tar -xvf /sources/expect5.45.tar.gz
 (
     cd expect5.45/
-    cp -vf configure{,.orig}
+    cp -v configure{,.orig}
     sed 's:/usr/local/bin:/bin:' configure.orig > configure
     ./configure --prefix=/tools \
         --with-tcl=/tools/lib \
@@ -268,9 +214,10 @@ tar -xvf $LFS/sources/expect5.45.tar.gz
     make test
     make SCRIPTS="" install
 )
+# rm -fr expect5.45/
 
 # 5.13. DejaGNU-1.6
-tar -xvf $LFS/sources/dejagnu-1.6.tar.gz
+# tar -xvf /sources/dejagnu-1.6.tar.gz
 (
     cd dejagnu-1.6/
     ./configure --prefix=/tools
@@ -278,9 +225,10 @@ tar -xvf $LFS/sources/dejagnu-1.6.tar.gz
     make install
     #    make check
 )
+# rm -fr dejagnu-1.6/
 
 # 5.14. Check-0.11.0
-tar -xvf $LFS/sources/check-0.11.0.tar.gz
+# tar -xvf /sources/check-0.11.0.tar.gz
 (
     cd check-0.11.0/
     PKG_CONFIG= ./configure --prefix=/tools
@@ -288,10 +236,11 @@ tar -xvf $LFS/sources/check-0.11.0.tar.gz
     #    make check
     make install 
 )
+# rm -fr check-0.11.0/
 
 
 # 5.15. Ncurses-6.0
-tar -xvf $LFS/sources/ncurses-6.0.tar.gz
+# tar -xvf /sources/ncurses-6.0.tar.gz
 (
     cd ncurses-6.0/
     gawk --version
@@ -305,10 +254,11 @@ tar -xvf $LFS/sources/ncurses-6.0.tar.gz
     make -j12
     make install
 )
+# rm -fr ncurses-6.0/
 
 
 # 5.16. Bash-4.4
-tar -xvf $LFS/sources/bash-4.4.tar.gz
+# tar -xvf /sources/bash-4.4.tar.gz
 (
     cd bash-4.4/
     ./configure --prefix=/tools --without-bash-malloc
@@ -317,9 +267,10 @@ tar -xvf $LFS/sources/bash-4.4.tar.gz
     make install
     ln -sv bash /tools/bin/sh
 )
+# rm -fr bash-4.4/
 
 # 5.17. Bison-3.0.4
-tar -xvf $LFS/sources/bison-3.0.4.tar.xz
+# tar -xvf /sources/bison-3.0.4.tar.xz
 (
     cd bison-3.0.4/
     ./configure --prefix=/tools
@@ -327,17 +278,19 @@ tar -xvf $LFS/sources/bison-3.0.4.tar.xz
     # make check
     make install
 )
+# rm -fr bison-3.0.4/
 
 # 5.18. Bzip2-1.0.6
-tar -xvf $LFS/sources/bzip2-1.0.6.tar.gz
+# tar -xvf /sources/bzip2-1.0.6.tar.gz
 (
     cd bzip2-1.0.6/
     make -j12
     make PREFIX=/tools install
 )
+# rm -fr bzip2-1.0.6/
 
 # 5.19. Coreutils-8.26
-tar -xvf $LFS/sources/coreutils-8.26.tar.xz
+# tar -xvf /sources/coreutils-8.26.tar.xz
 (
     cd coreutils-8.26/
     export FORCE_UNSAFE_CONFIGURE=1
@@ -346,9 +299,10 @@ tar -xvf $LFS/sources/coreutils-8.26.tar.xz
     # make RUN_EXPENSIVE_TESTS=yes check
     make install 
 )
+# rm -fr coreutils-8.26/
 
 # 5.20. Diffutils-3.5
-tar -xvf $LFS/sources/diffutils-3.5.tar.xz
+# tar -xvf /sources/diffutils-3.5.tar.xz
 (
     cd diffutils-3.5/
     ./configure --prefix=/tools
@@ -356,10 +310,11 @@ tar -xvf $LFS/sources/diffutils-3.5.tar.xz
     # make check
     make install
 )
+# rm -fr diffutils-3.5/
 
 
 # 5.21. File-5.30
-tar -xvf $LFS/sources/file-5.30.tar.gz
+# tar -xvf /sources/file-5.30.tar.gz
 (
     cd file-5.30/
     ./configure --prefix=/tools
@@ -367,9 +322,10 @@ tar -xvf $LFS/sources/file-5.30.tar.gz
     # make check
     make install
 )
+# rm -fr file-5.30/
 
 # 5.22. Findutils-4.6.0
-tar -xvf $LFS/sources/findutils-4.6.0.tar.gz
+# tar -xvf /sources/findutils-4.6.0.tar.gz
 (
     cd findutils-4.6.0/
     ./configure --prefix=/tools
@@ -377,9 +333,10 @@ tar -xvf $LFS/sources/findutils-4.6.0.tar.gz
     # make check
     make install    
 )
+# rm -fr findutils-4.6.0/
 
 # 5.23. Gawk-4.1.4
-tar -xvf $LFS/sources/gawk-4.1.4.tar.xz
+# tar -xvf /sources/gawk-4.1.4.tar.xz
 (
     cd gawk-4.1.4/
     ./configure --prefix=/tools
@@ -387,10 +344,11 @@ tar -xvf $LFS/sources/gawk-4.1.4.tar.xz
     # make check
     make install    
 )
+# rm -fr gawk-4.1.4/
 
 
 # 5.24. Gettext-0.19.8.1
-tar -xvf $LFS/sources/gettext-0.19.8.1.tar.xz
+# tar -xvf /sources/gettext-0.19.8.1.tar.xz
 (
     cd gettext-0.19.8.1/gettext-tools/
     EMACS="no" ./configure --prefix=/tools --disable-shared
@@ -399,11 +357,12 @@ tar -xvf $LFS/sources/gettext-0.19.8.1.tar.xz
     make -C src msgfmt -j12
     make -C src msgmerge -j12
     make -C src xgettext -j12
-    cp -vf src/{msgfmt,msgmerge,xgettext} /tools/bin
+    cp -v src/{msgfmt,msgmerge,xgettext} /tools/bin
 )
+# rm -fr gettext-0.19.8.1/
 
 # 5.25. Grep-3.0
-tar -xvf $LFS/sources/grep-3.0.tar.xz
+# tar -xvf /sources/grep-3.0.tar.xz
 (
     cd grep-3.0/
     ./configure --prefix=/tools
@@ -411,9 +370,10 @@ tar -xvf $LFS/sources/grep-3.0.tar.xz
     # make check
     make install    
 )
+# rm -fr grep-3.0/
 
 # 5.26. Gzip-1.8
-tar -xvf $LFS/sources/gzip-1.8.tar.xz
+# tar -xvf /sources/gzip-1.8.tar.xz
 (
     cd gzip-1.8/
     ./configure --prefix=/tools
@@ -421,9 +381,10 @@ tar -xvf $LFS/sources/gzip-1.8.tar.xz
     # make check
     make install    
 )
+# rm -fr gzip-1.8/
 
 # 5.27. M4-1.4.18
-tar -xvf $LFS/sources/m4-1.4.18.tar.xz
+# tar -xvf /sources/m4-1.4.18.tar.xz
 (
     cd m4-1.4.18/
     ./configure --prefix=/tools
@@ -431,9 +392,10 @@ tar -xvf $LFS/sources/m4-1.4.18.tar.xz
     # make check
     make install    
 )
+# rm -fr m4-1.4.18/
 
 # 5.28. Make-4.2.1
-tar -xvf $LFS/sources/make-4.2.1.tar.bz2
+# tar -xvf /sources/make-4.2.1.tar.bz2
 (
     cd make-4.2.1/
     ./configure --prefix=/tools --without-guile
@@ -441,9 +403,10 @@ tar -xvf $LFS/sources/make-4.2.1.tar.bz2
     # make check
     make install    
 )
+# rm -fr make-4.2.1/
 
 # 5.29. Patch-2.7.5
-tar -xvf $LFS/sources/patch-2.7.5.tar.xz
+# tar -xvf /sources/patch-2.7.5.tar.xz
 (
     cd patch-2.7.5/
     ./configure --prefix=/tools
@@ -451,20 +414,22 @@ tar -xvf $LFS/sources/patch-2.7.5.tar.xz
     # make check
     make install    
 )
+# rm -fr patch-2.7.5/
 
 # 5.30. Perl-5.24.1
-tar -xvf $LFS/sources/perl-5.24.1.tar.bz2
+# tar -xvf /sources/perl-5.24.1.tar.bz2
 (
     cd perl-5.24.1/
     sh Configure -des -Dprefix=/tools -Dlibs=-lm
     make -j12
-    cp -vf perl cpan/podlators/scripts/pod2man /tools/bin
+    cp -v perl cpan/podlators/scripts/pod2man /tools/bin
     mkdir -pv /tools/lib/perl5/5.24.1
-    cp -Rvf lib/* /tools/lib/perl5/5.24.1
+    cp -Rv lib/* /tools/lib/perl5/5.24.1
 )
+# rm -fr perl-5.24.1/
 
 # 5.31. Sed-4.4
-tar -xvf $LFS/sources/sed-4.4.tar.xz
+# tar -xvf /sources/sed-4.4.tar.xz
 (
     cd sed-4.4/
     ./configure --prefix=/tools
@@ -472,9 +437,10 @@ tar -xvf $LFS/sources/sed-4.4.tar.xz
     # make check
     make install    
 )
+# rm -fr sed-4.4/
 
 # 5.32. Tar-1.29
-tar -xvf $LFS/sources/tar-1.29.tar.xz
+# tar -xvf /sources/tar-1.29.tar.xz
 (
     cd tar-1.29/
     export FORCE_UNSAFE_CONFIGURE=1
@@ -483,9 +449,10 @@ tar -xvf $LFS/sources/tar-1.29.tar.xz
     # make check
     make install    
 )
+# rm -fr tar-1.29/
 
 # 5.33. Texinfo-6.3
-tar -xvf $LFS/sources/texinfo-6.3.tar.xz
+# tar -xvf /sources/texinfo-6.3.tar.xz
 (
     cd texinfo-6.3/
     ./configure --prefix=/tools
@@ -493,18 +460,20 @@ tar -xvf $LFS/sources/texinfo-6.3.tar.xz
     # make check
     make install
 )
+# rm -fr texinfo-6.3/
 
 # 5.34. Util-linux-2.29.1
-tar -xvf $LFS/sources/util-linux-2.29.1.tar.xz
+# tar -xvf /sources/util-linux-2.29.1.tar.xz
 (
     cd util-linux-2.29.1/
     ./configure --prefix=/tools --without-python --disable-makeinstall-chown --without-systemdsystemunitdir --enable-libmount-force-mountinfo PKG_CONFIG=""    
     make
     make install
 )
+# rm -fr util-linux-2.29.1/
 
 # 5.35. Xz-5.2.3
-tar -xvf $LFS/sources/xz-5.2.3.tar.xz
+# tar -xvf /sources/xz-5.2.3.tar.xz
 (
     cd xz-5.2.3/
     ./configure --prefix=/tools
@@ -512,13 +481,4 @@ tar -xvf $LFS/sources/xz-5.2.3.tar.xz
     # make check
     make install
 )
-
-#  5.36. Stripping
-strip --strip-debug /tools/lib/*
-/usr/bin/strip --strip-unneeded /tools/{,s}bin/*
-rm -rf /tools/{,share}/{info,man,doc}
-
-# Changing Ownership
-chown -R root:root $LFS/tools
-
 
